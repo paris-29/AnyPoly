358a6f08e87dcbdadaeea139d8f3d45c413761dd
Paris Nilesh Kulkarni <161126102+paris-29@users.noreply.github.com>
2025-12-10 21:28:47 +0530

    done some tasks

---
+++ Rust-basics/target/debug/Rust-basics.d
@@ -0,0 +1,1 @@
+/home/paris/Documents/AnyPoly/Rust-basics/target/debug/Rust-basics: /home/paris/Documents/AnyPoly/Rust-basics/src/main.rs
---
+++ Rust-basics/target/debug/deps/Rust_basics-71300f598885e8a8.d
@@ -0,0 +1,5 @@
+/home/paris/Documents/AnyPoly/Rust-basics/target/debug/deps/Rust_basics-71300f598885e8a8: src/main.rs
+
+/home/paris/Documents/AnyPoly/Rust-basics/target/debug/deps/Rust_basics-71300f598885e8a8.d: src/main.rs
+
+src/main.rs:

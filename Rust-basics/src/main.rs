a7f3bdb0bd1563c4e4a17f5ad75ac7023d7d002c
Paris Nilesh Kulkarni <161126102+paris-29@users.noreply.github.com>
2025-12-10 21:43:41 +0530

    done vars , data typess , func etc

--- Rust-basics/src/main.rs
+++ Rust-basics/src/main.rs
@@ -1,3 +1,15 @@
+fn welcome_func() {
+    println!("Welcome , do you know 2+2 = {} ", 2 + 2 ); //runing operations
+}
+
+fn vars() {
+    let a :i32 = 56;
+    let b :i32 = 44;
+    println!("if a = {a} and b = {b} , therefore {a} + {b} = {}", {a} + {b});
+}
+
 fn main(){
-    println!("HELLO , WORLD");
+    println!("hello User!");
+    welcome_func();
+    vars();
 }

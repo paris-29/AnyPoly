c73843e3b5efaf21a345ce84d09b941d243449b9
Paris Nilesh Kulkarni <161126102+paris-29@users.noreply.github.com>
2025-12-12 00:28:24 +0530

    :) added a simple calculator with my own logics , just a try tomrw i will make a bigger one

--- Rust-basics/src/main.rs
+++ Rust-basics/src/main.rs
@@ -1,3 +1,5 @@
+use std::io;
+
 fn welcome_func() {
     println!("Welcome , do you know 2+2 = {} ", 2 + 2 ); //runing operations
 }
@@ -8,8 +10,20 @@ fn vars() {
     println!("if a = {a} and b = {b} , therefore {a} + {b} = {}", {a} + {b});
 }
 
+fn var_simple_cal() {
+    let mut v1 = String::new();
+    let mut v2 = String::new();
+    io::stdin().read_line(&mut v2).expect("err");
+    io::stdin().read_line(&mut v1).expect("err reading");
+    let v1 : i32 = v1.trim().parse().expect("err converting");
+    let v2 : i32 = v2.trim().parse().expect("err");
+    println!("Multiplying {v1} with {v2} , so answer is {} ", {v1} * {v2} );
+
+}
+
 fn main(){
     println!("hello User!");
     welcome_func();
     vars();
+    var_simple_cal();
 }

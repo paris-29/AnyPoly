5cb68ab45535aedf87546409e38b5d28a22be4f2
Paris Nilesh Kulkarni <161126102+paris-29@users.noreply.github.com>
2025-12-14 21:55:20 +0530

    >:| yeh done little hard work on this thing , a good working calculator

--- Rust-basics/src/main.rs
+++ Rust-basics/src/main.rs
@@ -21,9 +21,59 @@ fn var_simple_cal() {
 
 }
 
+fn calculator() {
+println!("Starting Calculator");
+println!("\nhere are the Following operators,
+    1. '+' - to ADD values,
+    2. '-' - to SUB values,
+    3. '*' - to MULTIPLY values,
+    4. '/' - to DIVIDE values,
+    \n Write your EQN in the following format \n (num1) (opr) (num) " );
+
+println!("\n Enter eqn --> ");
+
+let mut inpt = String::new();
+
+io::stdin().read_line(&mut inpt).expect("err reading line");
+
+let inpt = inpt.trim();
+
+let prts : Vec<&str> = inpt.split_whitespace().collect();
+
+if prts.len() != 3 {
+    println!("Error fill in the correct format");
+    }
+
+let n1: f64 = match prts[0].parse() {
+    Ok(n) => n,
+    Err(_) => todo!(),
+};
+
+let n2: f64 = match prts[2].parse() {
+    Ok(n) => n,
+    Err(_) => todo!(),
+};
+
+let result = 
+if prts[1] == "+" {
+     n1 + n2 }
+else if prts[1] == "-" {
+    n1 - n2 }
+else if prts[1] == "*" {
+  n1 * n2 }
+else if prts[1] == "/" { 
+    n1 / n2 }
+else { 5.0 + 5.0 }; 
+println!("so {} {} {} = {}", n1, prts[1], n2, result );
+println!("Hello ");
+}
 fn main(){
     println!("hello User!");
     welcome_func();
     vars();
     var_simple_cal();
+    println!("ran till here");
+    calculator();
+    println!("ran till here");
+    //println!("so {} {} {} = {} ", n1, prts[1], n2, result );
 }
